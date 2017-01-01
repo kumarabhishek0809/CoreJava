05d406ae19649012f447292f1b488933c32860a5
kumar.abhishek <kumar.abhishek0809@gmail.com>
2017-01-01 23:04:01 +0400

    InnerClass

---
+++ src/core/InnerVariablesInMethod.java
@@ -0,0 +1,19 @@
+package core;
+
+import domain.Person;
+
+public class InnerVariablesInMethod {
+
+	public static void main(String[] args) {
+		InnerVariablesInMethod innerVariablesInMethod = new InnerVariablesInMethod();
+		innerVariablesInMethod.innerMethod(10, new Person(10,"Kumar"));
+
+	}
+
+	
+	private void innerMethod(final int age,Person person){
+		//Cant Change compilation Error age = 25;
+		person = new Person(11,"Can Change NP");
+		
+	}
+}
