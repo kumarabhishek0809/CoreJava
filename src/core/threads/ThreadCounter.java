4e86823442261381bfdc2f73c917090c7d43e7db
kumar.abhishek <kumar.abhishek0809@gmail.com>
2017-01-25 21:58:43 +0400

    test

--- src/core/threads/ThreadCounter.java
+++ src/core/threads/ThreadCounter.java
@@ -5,12 +5,11 @@ package core.threads;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadCounter {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i <10; i++) {
 			new Thread(new WorkerCounter()).start();
 		}
 	}
@@ -22,12 +21,12 @@ class WorkerCounter implements Runnable {
 	static private AtomicInteger counter = new AtomicInteger(0);
 	static Set<Integer> counterSet = new HashSet<Integer>();
 
-	static ReentrantLock counterLock = new ReentrantLock(true);
-
 	@Override
 	public void run() {
 		while (counter.get() < 1000) {
-			counter.incrementAndGet();
+			//synchronized(WorkerCounter.class){
+				counter.incrementAndGet();
+			//}
 			if (!counterSet.add(counter.get())) {
 				System.out.println(
 						Thread.currentThread().getName() + "   Duplicate Value :::: " + counter.get());
