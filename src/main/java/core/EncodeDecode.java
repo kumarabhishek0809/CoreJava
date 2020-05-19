f1b0d7da2ec0b14a1e8ae850c779576a2310fb97
kumar abhishek <mail2kumar.2021@gmail.com>
2020-05-19 11:57:24 +0400

    added new files

---
+++ src/main/java/core/EncodeDecode.java
@@ -0,0 +1,18 @@
+package core;
+
+import java.util.Base64;
+
+public class EncodeDecode {
+    public static void main(String[] args) {
+        String originalInput = "P@ssw0r$";
+        String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        System.out.println("encodedString ::::"+encodedString);
+
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        System.out.println("decodedString ::::"+decodedString);
+
+
+    }
+}
